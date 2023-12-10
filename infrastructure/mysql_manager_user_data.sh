36e02a8ef4ff03e1c0eb5b8ade0d1c1b3a9c4ec5
ming-xiao-yuan <ming-xiao.yuan@outlook.com>
2023-12-10 15:49:02 -0500

    when ip adresses are populated manager works but i need to find a way to populate the ip adresses

--- infrastructure/mysql_manager_user_data.sh
+++ infrastructure/mysql_manager_user_data.sh
@@ -1,49 +1,64 @@
 #!/bin/bash
+
+# Set environment variables for non-interactive frontend and PATH
 export DEBIAN_FRONTEND=noninteractive
 export PATH=/opt/mysqlcluster/home/mysqlc/bin:$PATH
 
+# Source the IP addresses from a separate file
+source ip_addresses.sh
+
 {
-    # Update the system
+    # Update and upgrade the system packages
     sudo apt-get update -y
     sudo apt-get upgrade -y
 
-    # Create a directory for MySQL Cluster
+    # Create a directory structure for MySQL Cluster
     sudo mkdir -p /opt/mysqlcluster/home
     cd /opt/mysqlcluster/home
 
-    MYSQL_CLUSTER_URL="https://downloads.mysql.com/archives/get/p/14/file/mysql-cluster-gpl-7.2.1-linux2.6-i686.tar.gz"
-    
+    # Download MySQL Cluster package
+    MYSQL_CLUSTER_URL="http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz"
     sudo wget $MYSQL_CLUSTER_URL
 
-    sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-i686.tar.gz
+    # Extract the downloaded package
+    sudo tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 
-    sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-i686 mysqlc
+    # Create a symbolic link to the extracted directory for easier reference
+    sudo ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 
+    # Set environment variables for MySQL Cluster and update the system PATH
     echo 'export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' | sudo tee /etc/profile.d/mysqlc.sh
-
     echo 'export PATH=$MYSQLC_HOME/bin:$PATH' | sudo tee -a /etc/profile.d/mysqlc.sh
     
+    # Reload the profile to apply the new PATH
     source /etc/profile.d/mysqlc.sh
 
+    # Install additional required packages
+    sudo apt-get update && sudo apt-get -y install libncurses5
+
+    # Create directories for configuration, data for MySQLD and NDB
     sudo mkdir -p /opt/mysqlcluster/deploy
-
     cd /opt/mysqlcluster/deploy
-
     sudo mkdir conf mysqld_data ndb_data
 
+    # Go to the configuration directory
     cd conf
 
-    # Create and write to my.cnf
+    # Create and write the MySQLD configuration file
     sudo bash -c 'cat <<EOF > /opt/mysqlcluster/deploy/conf/my.cnf
-        [mysqld]
-        ndbcluster
-        datadir=/opt/mysqlcluster/deploy/mysqld_data
-        basedir=/opt/mysqlcluster/home/mysqlc
-        port=3306
-        EOF'
+    [mysqld]
+    ndbcluster
+    datadir=/opt/mysqlcluster/deploy/mysqld_data
+    basedir=/opt/mysqlcluster/home/mysqlc
+    port=3306
+    EOF'
 
-    # Generate config.ini with dynamic IPs
-    MANAGER_IP_CONTENT="[ndb_mgmd]
+    # Display the Manager and Worker IPs for verification
+    echo "Manager IP: $MANAGER_IP"
+    echo "Worker IPs: ${WORKER_IPS[@]}"
+
+    # Prepare the content for the MySQL Cluster configuration file
+    CONFIG_INI_CONTENT="[ndb_mgmd]
     hostname=${MANAGER_IP}
     datadir=/opt/mysqlcluster/deploy/ndb_data
     nodeid=1
@@ -51,26 +66,33 @@ export PATH=/opt/mysqlcluster/home/mysqlc/bin:$PATH
     [ndbd default]
     noofreplicas=3
     datadir=/opt/mysqlcluster/deploy/ndb_data
+
+    [ndbd]
+    hostname=${WORKER_IPS[0]}
+    nodeid=2
+
+    [ndbd]
+    hostname=${WORKER_IPS[1]}
+    nodeid=3
+
+    [ndbd]
+    hostname=${WORKER_IPS[2]}
+    nodeid=4
+
+    [mysqld]
+    nodeid=50
     "
 
-    echo "$MANAGER_IP_CONTENT" | sudo tee /opt/mysqlcluster/deploy/conf/config.ini
+    # Write the MySQL Cluster configuration to the config.ini file
+    echo "$CONFIG_INI_CONTENT" | sudo tee /opt/mysqlcluster/deploy/conf/config.ini
 
-    # Add worker nodes
-    NODEID=2
-    for WORKER_IP in $WORKER_IPS; do
-        WORKER_IP_CONTENT="[ndbd]
-    hostname=${WORKER_IP}
-    nodeid=${NODEID}
-    "
-        echo "$WORKER_IP_CONTENT" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-        ((NODEID++))
-    done
-
-    # Append mysqld to config.ini
-    echo "[mysqld]
-    nodeid=50" | sudo tee -a /opt/mysqlcluster/deploy/conf/config.ini
-
+    # Go back to MySQLC directory
     cd /opt/mysqlcluster/home/mysqlc
 
+    # Initialize MySQL data directory
+    sudo scripts/mysql_install_db --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data
+
+    # Start the MySQL Cluster Management Node
+    sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/ --ndb-nodeid=1
 
 } >> /var/log/progress.log 2>&1
--- scripts/create_instances.sh
+++ scripts/create_instances.sh
@@ -17,11 +17,12 @@ terraform.exe plan -out=tfplan -var="AWS_ACCESS_KEY=$AWS_ACCESS_KEY" -var="AWS_S
 terraform.exe apply -auto-approve tfplan
 
 # Capture the IP addresses
-MANAGER_IP=$(terraform.exe output -raw mysql_cluster_manager_ip)
+export MANAGER_IP=$(terraform.exe output -raw mysql_cluster_manager_ip)
 
 # Assuming the worker IPs output is a list of IPs in a JSON array format
-WORKER_IPS=$(terraform.exe output -json mysql_cluster_worker_ips | grep -oP '(?<=\[|\,)\s*"\K[^"]+')
+export WORKER_IPS=$(terraform.exe output -json mysql_cluster_worker_ips | grep -oP '(?<=\[|\,)\s*"\K[^"]+')
 
-# Export IPs to be used in the user data script
-export MANAGER_IP
-export WORKER_IPS
+# Export the IPs to a file
+echo "MANAGER_IP=$MANAGER_IP" > ip_addresses.sh
+echo "WORKER_IPS=(${WORKER_IPS[@]})" >> ip_addresses.sh
+
